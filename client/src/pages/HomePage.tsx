fda2c500f64fd10889fbdf37f41eb19374c9e546
Arni1197 <ali_74@mail.ru>
2025-11-07 17:56:15 +0300

    Краткое описание изменений

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-const HomePage = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-}
-
-export default HomePage;
+export const HomePage = () => {
+  return <div></div>;
+};
