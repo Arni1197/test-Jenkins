3b725ef387d1c20c1a68534b0049783c44b145c5
Arni1197 <ali_74@mail.ru>
2025-11-24 23:50:21 +0300

    Fix last Mongo in project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,19 +2,14 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { RedisModule } from './modules/redis/redis.module';
 import { envValidationSchema } from './config/env.validation';
 import { ValidationExceptionFilter } from './common/filters/validation-exception.filter';
-import { MongoExceptionFilter } from './common/filters/mongo-exception.filter';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { PrismaModule } from './prisma/prisma.module';
-
-
 
 @Module({
   imports: [
@@ -29,36 +24,20 @@ import { PrismaModule } from './prisma/prisma.module';
       envFilePath: '.env',
       validationSchema: envValidationSchema,
     }),
-    MongooseModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (cfg: ConfigService) => ({
-        uri: cfg.get<string>('MONGO_URI') || 'mongodb://mongo:27017/game-db',
-      }),
-    }),
     RedisModule,
     AuthModule,
     UsersModule,
     PrismaModule,
-    
   ],
   providers: [
-    // порядок: от более специфичных к самому общему
     {
       provide: APP_FILTER,
       useClass: ValidationExceptionFilter,
     },
     {
       provide: APP_FILTER,
-      useClass: MongoExceptionFilter,
-    },
-    {
-      provide: APP_FILTER,
       useClass: HttpExceptionFilter,
     },
-    // {
-    //   provide: APP_FILTER,
-    //   useClass: AllExceptionsFilter,
-    // },
   ],
 })
 export class AppModule {}
