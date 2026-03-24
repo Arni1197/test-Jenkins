9901119ca75dee525f24b3baf8dc030b00cd2298
Arni1197 <ali_74@mail.ru>
2026-03-24 12:36:03 +0300

    TER_1

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,20 +1,20 @@
 data "twc_k8s_preset" "master" {
-  cpu  = 4
+  cpu  = var.master_cpu
   type = "master"
 }
 
 data "twc_k8s_preset" "apps" {
-  cpu  = 4
+  cpu  = var.apps_cpu
   type = "worker"
 }
 
 data "twc_k8s_preset" "infra" {
-  cpu  = 2
+  cpu  = var.infra_cpu
   type = "worker"
 }
 
 data "twc_k8s_preset" "monitoring" {
-  cpu  = 4
+  cpu  = var.monitoring_cpu
   type = "worker"
 }
 
@@ -25,25 +25,41 @@ resource "twc_k8s_cluster" "main" {
   ingress        = var.ingress_enabled
 
   preset_id = data.twc_k8s_preset.master.id
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "twc_k8s_node_group" "apps" {
   cluster_id = twc_k8s_cluster.main.id
-  name       = "apps-pool"
+  name       = var.apps_pool_name
   preset_id  = data.twc_k8s_preset.apps.id
   node_count = var.apps_nodes_count
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "twc_k8s_node_group" "infra" {
   cluster_id = twc_k8s_cluster.main.id
-  name       = "infra-pool"
+  name       = var.infra_pool_name
   preset_id  = data.twc_k8s_preset.infra.id
   node_count = var.infra_nodes_count
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "twc_k8s_node_group" "monitoring" {
   cluster_id = twc_k8s_cluster.main.id
-  name       = "monitoring-pool"
+  name       = var.monitoring_pool_name
   preset_id  = data.twc_k8s_preset.monitoring.id
   node_count = var.monitoring_nodes_count
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
