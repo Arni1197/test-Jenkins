0a6e79c01d10170b016edba16062482bd8772f91
Arni1197 <ali_74@mail.ru>
2026-03-29 19:15:56 +0300

    feat: add postgres database instances

--- terraform/cloud/postgres.tf
+++ terraform/cloud/postgres.tf
@@ -27,3 +27,14 @@ resource "twc_database_cluster" "audit_postgres" {
   type      = "postgres16"
   preset_id = data.twc_database_preset.audit_postgres_preset.id
 }
+
+
+resource "twc_database_instance" "app_db" {
+  cluster_id = twc_database_cluster.app_postgres.id
+  name       = "app_db"
+}
+
+resource "twc_database_instance" "audit_db" {
+  cluster_id = twc_database_cluster.audit_postgres.id
+  name       = "audit_db"
+}
