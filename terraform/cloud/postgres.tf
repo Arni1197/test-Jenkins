26292ea805708d8a27c287d0be46b1116f5108f3
Arni1197 <ali_74@mail.ru>
2026-03-29 18:11:46 +0300

    fix postgres type to postgres16

--- terraform/cloud/postgres.tf
+++ terraform/cloud/postgres.tf
@@ -1,6 +1,6 @@
 data "twc_database_preset" "app_postgres_preset" {
   location = var.region
-  type     = "postgres"
+  type     = "postgres16"
 
   cpu  = 2
   ram  = 4096
@@ -9,13 +9,13 @@ data "twc_database_preset" "app_postgres_preset" {
 
 resource "twc_database_cluster" "app_postgres" {
   name      = "app-postgres"
-  type      = "postgres"
+  type      = "postgres16"
   preset_id = data.twc_database_preset.app_postgres_preset.id
 }
 
 data "twc_database_preset" "audit_postgres_preset" {
   location = var.region
-  type     = "postgres"
+  type     = "postgres16"
 
   cpu  = 2
   ram  = 4096
@@ -24,6 +24,6 @@ data "twc_database_preset" "audit_postgres_preset" {
 
 resource "twc_database_cluster" "audit_postgres" {
   name      = "audit-postgres"
-  type      = "postgres"
+  type      = "postgres16"
   preset_id = data.twc_database_preset.audit_postgres_preset.id
 }
