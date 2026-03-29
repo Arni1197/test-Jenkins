122ae4b5bfb7620fa516cfa7435a117b8e3cc19f
Arni1197 <ali_74@mail.ru>
2026-03-29 18:00:42 +0300

    fix timeweb postgres cluster config

--- terraform/cloud/postgres.tf
+++ terraform/cloud/postgres.tf
@@ -1,66 +1,29 @@
-# -------------------------------
-# BUSINESS POSTGRES
-# -------------------------------
+data "twc_database_preset" "app_postgres_preset" {
+  location = var.region
+  type     = "postgres"
+
+  cpu  = 2
+  ram  = 4096
+  disk = 40 * 1024
+}
+
 resource "twc_database_cluster" "app_postgres" {
-  name     = "app-postgres"
-  type     = "postgresql"
-  version  = var.postgres_version
-  region   = var.region
-
-  configuration {
-    cpu    = 2
-    ram    = 4096
-    disk   = 40
-  }
+  name      = "app-postgres"
+  type      = "postgres"
+  preset_id = data.twc_database_preset.app_postgres_preset.id
 }
 
-resource "twc_database_database" "app_db" {
-  cluster_id = twc_database_cluster.app_postgres.id
-  name       = "app_db"
+data "twc_database_preset" "audit_postgres_preset" {
+  location = var.region
+  type     = "postgres"
+
+  cpu  = 2
+  ram  = 4096
+  disk = 40 * 1024
 }
 
-resource "twc_database_user" "app_user" {
-  cluster_id = twc_database_cluster.app_postgres.id
-  name       = "app_user"
-  password   = random_password.app.result
-}
-
-# -------------------------------
-# AUDIT POSTGRES
-# -------------------------------
 resource "twc_database_cluster" "audit_postgres" {
-  name     = "audit-postgres"
-  type     = "postgresql"
-  version  = var.postgres_version
-  region   = var.region
-
-  configuration {
-    cpu    = 2
-    ram    = 4096
-    disk   = 40
-  }
-}
-
-resource "twc_database_database" "audit_db" {
-  cluster_id = twc_database_cluster.audit_postgres.id
-  name       = "audit_db"
-}
-
-resource "twc_database_user" "audit_user" {
-  cluster_id = twc_database_cluster.audit_postgres.id
-  name       = "audit_user"
-  password   = random_password.audit.result
-}
-
-# -------------------------------
-# PASSWORDS
-# -------------------------------
-resource "random_password" "app" {
-  length  = 20
-  special = true
-}
-
-resource "random_password" "audit" {
-  length  = 20
-  special = true
+  name      = "audit-postgres"
+  type      = "postgres"
+  preset_id = data.twc_database_preset.audit_postgres_preset.id
 }
