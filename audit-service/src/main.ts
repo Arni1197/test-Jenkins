2ee7d7f3451c79243f8d7ec70fcaa8be3bb84f9c
Arni1197 <ali_74@mail.ru>
2026-04-23 15:52:11 +0300

    run audit service HTTP server on port 3000

--- audit-service/src/main.ts
+++ audit-service/src/main.ts
@@ -1,13 +1,17 @@
 import 'reflect-metadata';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
 
-  const port = Number(process.env.PORT ?? 3004);
+  const port = Number(process.env.PORT ?? 3000);
   await app.listen(port, '0.0.0.0');
 
-  console.log(`🚀 Audit Service started on http://0.0.0.0:${port}`);
+  logger.log(`🚀 Audit Service started on http://0.0.0.0:${port}`);
 }
+
 bootstrap();
