7b84d5dcb96e6d6706e07c3f553f177fa0868fa6
Arni1197 <ali_74@mail.ru>
2026-05-25 15:30:31 +0300

    fix audit module metrics injection

--- user-service/src/audit/audit.module.ts
+++ user-service/src/audit/audit.module.ts
@@ -1,10 +1,19 @@
 // user-service/src/audit/audit.module.ts
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { KafkaModule } from '../kafka/kafka.module';
+import { MetricsModule } from '../metrics/metrics.module';
+
 import { AuditEventsService } from './audit-events.service';
 
 @Module({
-  imports: [KafkaModule],
+  imports: [
+    ConfigModule,
+    KafkaModule,
+    MetricsModule,
+  ],
   providers: [AuditEventsService],
   exports: [AuditEventsService],
 })
